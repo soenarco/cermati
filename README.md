c51f4c5172635417f35e601dd355df61393fc902
soenarko <sunarko@misteraladin.com>
2020-04-13 20:38:33 +0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # cermati
 Front End test cermati.com 
+
+This application was created using the Vue.js Framework namely Nuxt.js. You can read the documentation at https://nuxtjs.org/
+
+How to run project :
+1. npm install
+2. npm run dev
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <main id="site-content">
+    Cermati.com
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'Homepage',
+
+  head() {
+    return {
+      title: 'Sunarko | Cermati.com Front-end Developer Entry Test',
+      bodyAttrs: {
+        class: 'page-home',
+      },
+    };
+  },
+};
+</script>
